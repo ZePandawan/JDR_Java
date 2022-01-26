1fb0af4d3d3408cf421117540435bda7ea837cb1
Vincent_Reix_54 <44094925+ZePandawan@users.noreply.github.com>
2022-01-26 23:34:46 +0100

    Create infos_persos.java

---
+++ infos_persos.java
@@ -0,0 +1,14 @@
+
+public class infos_perso {
+	String nom;
+	int classe;
+	int age;
+	int stats1;
+	int stats2;
+	int stats3;
+
+	public infos_perso() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
