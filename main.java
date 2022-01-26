800f6b3c151a775ed052900caab4014fe72babb3
Vincent_Reix_54 <44094925+ZePandawan@users.noreply.github.com>
2022-01-26 23:33:27 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		create_perso test = new create_perso();
+		System.out.println(test.toString());
+	}
+
+}
