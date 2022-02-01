507fa55676a5c9c01c989d6dd98e5e7f1cfacbfb
Vincent_Reix_54 <44094925+ZePandawan@users.noreply.github.com>
2022-02-01 14:27:01 +0100

    New version

--- create_perso.java
+++ create_perso.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class create_perso {
 	String name_aventurier;
 	int age;
+	String race;
+	String [] tab_classe = {} ;
+	String [] tab_race = {"Hobbit","Humain","Elfe","Nain","Ent","Malar","Orque","Uruk-hai","Nazgul","Gobelin","Troll"} ;
+	
 	public create_perso() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Bonjour Cher Joueur ! \n ");
@@ -11,13 +15,21 @@ public class create_perso {
 		System.out.print( "Veuillez entrer votre nom cher aventurier : " );
 		this.name_aventurier = scanner.nextLine();
 		System.out.print("-------------------- \n");
-		System.out.print("| 1 |    test      | \n");
-		System.out.print("| 2 |   je teste   | \n");
-		System.out.print("| 3 |  Ptn je suis | \n");
-		System.out.print("| 4 |   chauuuud   | \n");
-		System.out.print("| 5 |     bite     | \n");
-		System.out.print("| 6 |     suce     | \n");
+		System.out.print("| 1 |    Hobbit    | \n");
+		System.out.print("| 2 |    Humain    | \n");
+		System.out.print("| 3 |     Elfe     | \n");
+		System.out.print("| 4 |     Nain     | \n");
+		System.out.print("| 5 |     Ent      | \n");
+		System.out.print("| 6 |    Malar     | \n");
+		System.out.print("| 7 |    Orque     | \n");
+		System.out.print("| 8 |    Uruk-ai   | \n");
+		System.out.print("| 9 |    Nazgul    | \n");
+		System.out.print("|10 |    Gobelin   | \n");
+		System.out.print("|11 |     Troll    | \n");
 		System.out.print("-------------------- \n");
+		System.out.println("Quel est votre race "+ this.name_aventurier + " ? ");
+		int race_choisie = scanner.nextInt();
+		this.race = this.tab_race[race_choisie-1];
 		System.out.println("Quel est votre age "+ this.name_aventurier + " ? ");
 		this.age = scanner.nextInt();
 		
@@ -25,7 +37,7 @@ public class create_perso {
 	
 	
 	public String toString() {
-		return this.name_aventurier + this.age ;
+		return this.name_aventurier + " " + this.age + " " + this.race;
 	}
 
 }
