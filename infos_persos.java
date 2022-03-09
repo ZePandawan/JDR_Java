b15c8cd0e4ce154dc203dc1cc0eefb6fa040e47a
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-03-09 14:36:18 +0100

    Update New Save System

--- infos_persos.java
+++ infos_persos.java
@@ -1,6 +1,7 @@
 public class infos_persos {
 	private String nom;
-	private int classe;
+	private String classe;
+	private String race;
 	private int age;
 	private String Competence_1;
 	private String Competence_2;
@@ -10,6 +11,47 @@ public class infos_persos {
 	public infos_persos() {
 		
 	}
+
+	public String getNom()
+	{
+		return this.nom;
+	}
+
+	public String getClasse()
+	{
+		return this.classe;
+	}
+
+	public String getRace()
+	{
+		return this.race;
+	}
+
+	public int getAge()
+	{
+		return this.age;
+	}
+
+	public String getComp1()
+	{
+		return this.Competence_1;
+	}
+
+	public String getComp2()
+	{
+		return this.Competence_2;
+	}
+
+	public String getComp3()
+	{
+		return this.Competence_3;
+	}
+
+	public String [] getInv()
+	{
+		return this.inventory;
+	}
+
 
 }
 
--- sauvegarde.java
+++ sauvegarde.java
@@ -1,3 +1,6 @@
 public class sauvegarde {
-        
+        public sauvegarde(){
+            infos_persos save = new infos_persos();
+            
+        }
 }
