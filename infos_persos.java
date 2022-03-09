a763c26cfbc268ad014f2ed5f5d65f94ea598250
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-03-09 14:30:38 +0100

    blabla

--- infos_persos.java
+++ infos_persos.java
@@ -1,14 +1,14 @@
 public class infos_persos {
-	String nom;
-	int classe;
-	int age;
-	int stats1;
-	int stats2;
-	int stats3;
-	String [] inventory;
+	private String nom;
+	private int classe;
+	private int age;
+	private String Competence_1;
+	private String Competence_2;
+	private String Competence_3;
+	private String [] inventory;
 
 	public infos_persos() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- main.java
+++ main.java
@@ -8,6 +8,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		create_perso test = new create_perso();
+		infos_persos haha = new infos_persos();
 		System.out.println(test.toString());
 	}
 
