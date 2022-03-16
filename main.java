a82de075ae3f69a02be77d28a2c0de73ffc721c1
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-03-16 18:06:09 +0100

    Update main.java

--- main.java
+++ main.java
@@ -13,7 +13,7 @@ public class main {
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
 		create_perso test = new create_perso();
-		infos_persos haha = new infos_persos();
+		//infos_persos haha = new infos_persos();
 		sauvegarde test2 = new sauvegarde();
 		test2.inFile();
 		System.out.println(test.toString());
