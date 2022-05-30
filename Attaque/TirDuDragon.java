2d3e87da1121e48e114b302ce9ada3d1cbf29090
xadlis <98535251+xadlis@users.noreply.github.com>
2022-05-30 08:07:41 +0200

    Add files via upload

---
+++ Attaque/TirDuDragon.java
@@ -0,0 +1,36 @@
+package Attaque;
+import Classes.Classe;
+import Joueur.Joueur;
+
+public class TirDuDragon extends Attaque{
+	public TirDuDragon(Classe classe) {
+		super(classe);
+		_description = "RYUU GA WAGA TEKI WO KURAU (2 Mana)";
+		_nom = "Tir du dragon";
+		_type = "Magique";
+	}
+	
+	public int ValeurAttaque(Joueur joueur) {
+		if(joueur.getMana() < 2)
+		{
+			System.out.println("Vous n'avez pas assez de mana ! ");
+			return 0;
+		}
+		else
+		{
+			if((_classe.getMental() - 10) > random.nextInt(1, 100))
+			{
+				joueur.EnleverMana(2);
+				_degats = _classe.getClasseDegats() + _classe.getArmeDegatsM() * 2;
+				return _degats;
+			}
+			else
+			{
+				joueur.EnleverMana(2);
+				System.out.println("Votre attaque n'a pas aboutit");
+				return 0;
+			}
+		}
+	}
+
+}
---
+++ Equipements/Epeeadeuxmains.java
@@ -0,0 +1,11 @@
+package Equipements;
+
+public class Epeeadeuxmains extends Equipement{
+    public Epeeadeuxmains() {
+        degatP = 5;
+        degatM = 4;
+        armure = 0;
+        prix = 9;
+        nom = "Épée à deux mains";
+    }
+}
