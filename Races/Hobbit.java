1df30198e390ebf19c26f2a962d693541496bd09
Vincent_Reix_54 <44094925+ZePandawan@users.noreply.github.com>
2022-06-01 20:12:35 +0200

    Delete Races directory

--- Races/Hobbit.java
+++
@@ -1,8 +0,0 @@
-package Races;
-
-public class Hobbit extends Race {
-	
-	public Hobbit() {
-		Description = "les Hobbits sont des créatures de petite taille, préférant le confort de leur maison, cependant certaines histoires racontent les aventures d'un groupe de Hobbits.";
-	}
-}
--- Races/Nain.java
+++
@@ -1,8 +0,0 @@
-package Races;
-
-public class Nain extends Race {
-	
-	public Nain() {
-		Description = "les nains sont des individus de petites tailles mais pouvant être très robuste ! Leur fort caractère et leur franc parler font d'eux une communauté particulière.";
-	}
-}
