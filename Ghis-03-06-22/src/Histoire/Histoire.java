694a2645a5aa317d773c3ddc75654c6a593d96c6
xadlis <98535251+xadlis@users.noreply.github.com>
2022-06-03 13:13:10 +0200

    Add files via upload

---
+++ Ghis-03-06-22/src/Histoire/Histoire.java
@@ -0,0 +1,51 @@
+package Histoire;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import Commandes.*;
+import Joueur.Joueur;
+
+public class Histoire {
+	
+	protected static Scanner scanner = new Scanner(System.in);
+	protected Jeu Commande = new Jeu();
+	protected Random random = new Random();
+
+	
+	public Joueur DefPersonnage(Joueur joueur)
+	{
+		String nom;
+		
+		Commande.NettoyerConsole();
+		Commande.AfficherEntete(122, "\nAragorn : Quel est votre nom mon brave ?!\n");
+		System.out.print("-> Nom : ");
+		nom = scanner.nextLine();
+
+		
+		Commande.NettoyerConsole();
+		Commande.AfficherEntete(122,"\nAragorn : Votre nom est " + nom + ". N'est-ce pas ? \n\n"
+				+ "[1] Oui ! \n"
+				+ "[2] Non ! \n");
+		int choix = Commande.LectureInt("->", 2);
+		
+		
+		while(choix == 2)
+		{
+			Commande.NettoyerConsole();
+			Commande.AfficherEntete(122, "\nAragorn : Quel est votre nom mon brave ?!\n");
+			System.out.print("-> Nom : ");
+			nom = scanner.nextLine();
+
+			
+			Commande.NettoyerConsole();
+			Commande.AfficherEntete(122,"\nAragorn : Votre nom est " + nom + ". N'est-ce pas ? \n\n"
+					+ "[1] Oui ! \n"
+					+ "[2] Non ! \n");
+			choix = Commande.LectureInt("->", 2);
+		}
+		
+		joueur = new Joueur(nom);
+		return joueur;
+	}
+}
