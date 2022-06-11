8b6a8b31009e9048c5a0f81b29ca487014752c40
xadlis <98535251+xadlis@users.noreply.github.com>
2022-06-11 18:42:58 +0200

    Delete Ghis-11-06-22/src/Javadoc directory

--- Ghis-11-06-22/src/Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Attaque"},{"l":"Classes"},{"l":"Commandes"},{"l":"Consommables"},{"l":"Equipements"},{"l":"Histoire"},{"l":"Jeu"},{"l":"Joueur"},{"l":"Monstres"},{"l":"Races"},{"l":"Terrain"}];updateSearchResults();
